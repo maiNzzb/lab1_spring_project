f7371a5c708627331e74c41f578343427b66c304
maiNzzb <andreycs29@gmail.com>
2020-10-10 00:35:26 +0300

    Initial commit

---
+++ src/main/java/sut/ist813/Kukin/test_spring_project/TestSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package sut.ist813.Kukin.test_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sut/ist813/Kukin/test_spring_project/TestSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sut.ist813.Kukin.test_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
